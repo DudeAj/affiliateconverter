b2fd88fbb08a53f8eed9724ed090c7465d7c7ba1
Ajay Kumar <ajaygangwar@gmail.com>
2024-02-19 23:06:15 +0530

    Added the views configuration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+app.set("views", __dirname + "/views");
 app.set("view engine", 'ejs');
+app.use(express.static(__dirname + "public"));
 const fetch = require('node-fetch');
 const Bluebird = require('bluebird');
 fetch.Promise = Bluebird;
